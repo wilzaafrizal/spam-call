29ef3d414cb6b1a4078381541f1c3b2898de5736
wilzaafrizal <89372553+wilzaafrizal@users.noreply.github.com>
2021-11-12 08:01:43 +0700

    Add files via upload

---
+++ fastspeed.sh
@@ -0,0 +1,37 @@
+#kalo kalian mau recode gpp
+#asalkan buat belajar ya jgn sampai disalahgunakan
+#author BlueeWlz
+
+clear
+sleep 2
+echo
+toilet -f big -F gay Bye
+echo $green"Selamat Datang Di Tools"
+echo
+sleep 3
+echo $yellow"Silahkan Pilih :"
+echo
+echo $cyan"[1] Stabilize network"
+echo
+echo $blue"[2] Exit tools"
+echo
+echo $white
+read -p "pilih : " bro
+if [ $bro = 1 ] || [$bro = 1 ] 
+then
+clear
+toilet -f big -F gay Bluee Wlz
+echo $green"Stabilize network by Bluee Wlz"
+sleep 3
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"To be continued"
+sleep 4
+exit
+fi
